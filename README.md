6ef4d85a63e13ded46b9d6c3fa742e0876c9aa2e
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2019-08-07 19:32:01 +0200

    Add base files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Top Menu Products for Magento 2
+
+## Overview
+
+Top Menu Products for Magento 2 enables you to easily add products to your Top Menu instead of just categories.
+
+## Features
+
+To be continued...
---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author: Daan van den Bergh
+ * @url: https://daan.dev
+ * @package: Dan0sz/TopMenuProducts
+ * @copyright: (c) 2019 Daan van den Bergh
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Dan0sz_TopMenuProducts',
+    __DIR__
+);
