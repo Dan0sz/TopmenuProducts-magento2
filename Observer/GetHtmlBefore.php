509db9de4b6757e86e55b4578e9d59ec5229d148
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2019-08-08 16:08:50 +0200

    Added observer for top menu products

---
+++ Observer/GetHtmlBefore.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author   : Daan van den Bergh
+ * @url      : https://daan.dev
+ * @package  : Dan0sz/TopMenuProducts
+ * @copyright: (c) 2019 Daan van den Bergh
+ */
+
+namespace Dan0sz\TopMenuProducts\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+class GetHtmlBefore implements ObserverInterface
+{
+    public function execute(
+        Observer $observer
+    ) {
+        // TODO: Add logic for Top Menu Products.
+    }
+}
