7c417265b9fd673665bb2bd1de738403339a4ad0
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2019-08-09 16:23:49 +0200

    Added PHPdocs and some re-factors to improve readability

--- Observer/GetHtmlBefore.php
+++ Observer/GetHtmlBefore.php
@@ -11,6 +11,7 @@ namespace Dan0sz\TopMenuProducts\Observer;
 use Dan0sz\TopMenuProducts\Model\Config;
 use Magento\Catalog\Model\ResourceModel\Product\Collection as ProductCollection;
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
+use Magento\Framework\App\Request\Http;
 use Magento\Framework\Data\Tree\Node;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
@@ -18,31 +19,49 @@ use Magento\Store\Model\StoreManagerInterface;
 
 class GetHtmlBefore implements ObserverInterface
 {
+    /** @var ProductCollection $productCollection */
     private $productCollection;
 
+    /** @var StoreManagerInterface $storeManager */
     private $storeManager;
 
+    /** @var Http $request */
     private $request;
 
+    /**
+     * GetHtmlBefore constructor.
+     *
+     * @param ProductCollectionFactory $productCollection
+     * @param StoreManagerInterface    $storeManager
+     */
     public function __construct(
         ProductCollectionFactory $productCollection,
         StoreManagerInterface $storeManager
     ) {
         $this->productCollection = $productCollection;
-        $this->storeManager = $storeManager;
+        $this->storeManager      = $storeManager;
     }
 
-    public function execute(
-        Observer $observer
-    ) {
-        $menu = $observer->getMenu();
-        $this->request = $observer->getRequest();
-        $products = $this->getTopMenuProducts();
+    /**
+     * @param Observer $observer
+     *
+     * @return $this|void
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function execute(Observer $observer) {
+        /** @var Node $menu */
+        $menu          = $observer->getData('menu');
+        /** @var Http request */
+        $this->request = $observer->getData('request');
+        $products      = $this->getTopMenuProducts();
         $this->addProductsToMenu($products, $menu);
 
         return $this;
     }
 
+    /**
+     * @return ProductCollection
+     */
     private function getTopMenuProducts()
     {
         /** @var ProductCollection $productCollection */
@@ -59,17 +78,20 @@ class GetHtmlBefore implements ObserverInterface
                 Config::ATTR_TOP_MENU_PRODUCT_IS_HOME
             ]
         );
-        $productCollection->addAttributeToFilter(Config::ATTR_TOP_MENU_PRODUCT_ENABLED, ['eq' => 1]);
-        $productCollection->load();
+        $productCollection->addAttributeToFilter(
+            Config::ATTR_TOP_MENU_PRODUCT_ENABLED, ['eq' => 1]
+        );
 
         return $productCollection;
     }
 
     /**
      * @param ProductCollection $products
-     * @param \Magento\Framework\Data\Tree\Node $menu
+     * @param Node              $menu
+     *
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
      */
-    private function addProductsToMenu($products, $menu)
+    private function addProductsToMenu(ProductCollection $products, Node $menu)
     {
         foreach ($products as $product) {
             $nodeId = 'product-node-' . $product->getId();
@@ -85,27 +107,45 @@ class GetHtmlBefore implements ObserverInterface
         }
     }
 
-    private function getProductAsArray($product)
+    /**
+     * @param \Magento\Catalog\Model\Product $product
+     *
+     * @return array
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    private function getProductAsArray(\Magento\Catalog\Model\Product $product)
     {
         return [
-            'name' => $product->getTopMenuProductLabel() ?: $product->getName(),
-            'id' => 'product-node-' . $product->getId(),
-            'url' => $this->getTopMenuProductUrl($product),
-            'has_active' => false,
-            'is_active' => $this->checkIsActive($product),
-            'is_category' => false,
+            'name'             => $product->getData(Config::ATTR_TOP_MENU_PRODUCT_LABEL) ?: $product->getName(),
+            'id'               => 'product-node-' . $product->getId(),
+            'url'              => $this->getTopMenuProductUrl($product),
+            'has_active'       => false,
+            'is_active'        => $this->checkIsActive($product),
+            'is_category'      => false,
             'is_parent_active' => true
         ];
     }
 
-    private function getTopMenuProductUrl($product)
+    /**
+     * @param \Magento\Catalog\Model\Product $product
+     *
+     * @return string
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    private function getTopMenuProductUrl(\Magento\Catalog\Model\Product $product)
     {
+        /** @var \Magento\Store\Model\Store $store */
         $store = $this->storeManager->getStore();
 
-        return $product->getTopMenuProductIsHome() ? $store->getBaseUrl() : $product->getProductUrl();
+        return $product->getData(Config::ATTR_TOP_MENU_PRODUCT_IS_HOME) ? $store->getBaseUrl() : $product->getProductUrl();
     }
 
-    private function checkIsActive($product)
+    /**
+     * @param \Magento\Catalog\Model\Product $product
+     *
+     * @return bool
+     */
+    private function checkIsActive(\Magento\Catalog\Model\Product $product)
     {
         return $product->getId() == $this->request->getParam('id');
     }
